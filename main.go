e8233bcf78667bb6d2edc033c935685008d141e0
Muhamad Ulil Absor <38712698+absormu@users.noreply.github.com>
2022-04-21 12:58:13 +0700

    Update main.go

--- main.go
+++ main.go
@@ -7,6 +7,6 @@ import (
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
-	log.Println("listening on port 3000...")
+	log.Println("start listening on port 3000...")
 	http.ListenAndServe(":3000", nil)
 }
